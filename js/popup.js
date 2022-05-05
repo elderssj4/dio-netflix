d5b19119fc5922a810d983c8e65a1f94de426450
Elder Duarte <elderssj4@gmail.com>
2022-05-05 00:30:47 -0300

    criando os outros carrosseis e adicionando link e popup aos botoes

---
+++ js/popup.js
@@ -0,0 +1,3 @@
+function newPopup(){
+    varWindow = window.open('popup.html', 'popup', "widht=400, height=600, top=100, lefe=110, scrollbars=no, resizable=false, menubar=no, location=no, toolbar=no, status=no")
+}
